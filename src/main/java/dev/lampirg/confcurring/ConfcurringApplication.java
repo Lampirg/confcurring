195f4b13d7e66209540d0098640fc843dce4ba2b
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-31 00:47:57 +0300

    init

---
+++ src/main/java/dev/lampirg/confcurring/ConfcurringApplication.java
@@ -0,0 +1,13 @@
+package dev.lampirg.confcurring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfcurringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfcurringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lampirg/confcurring/ConfcurringApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.lampirg.confcurring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfcurringApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertDoesNotThrow(() -> {});
+	}
+
+}
