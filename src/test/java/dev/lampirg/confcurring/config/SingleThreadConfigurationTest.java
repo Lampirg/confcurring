0ab9821edec46ba54fff895242b560bac1ee62d4
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-31 13:25:40 +0300

    create SingleThreadConfigurationTest

---
+++ src/test/java/dev/lampirg/confcurring/config/SingleThreadConfigurationTest.java
@@ -0,0 +1,38 @@
+package dev.lampirg.confcurring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = "concurrent.single-thread=true")
+class SingleThreadConfigurationTest {
+
+    @Autowired
+    @Qualifier("exportExecutor")
+    private TaskExecutor exportExecutor;
+
+    @Autowired
+    @Qualifier("importExecutor")
+    private TaskExecutor importExecutor;
+
+    @Autowired
+    private TaskExecutor someTaskExecutorWithNameThatNeverWillBeQualifier;
+
+    @Test
+    void isSync() {
+        assertEquals(exportExecutor.getClass(), SyncTaskExecutor.class);
+        assertEquals(importExecutor.getClass(), SyncTaskExecutor.class);
+        assertEquals(someTaskExecutorWithNameThatNeverWillBeQualifier.getClass(), SyncTaskExecutor.class);
+    }
+
+    @Test
+    void isSame() {
+        assertSame(exportExecutor, importExecutor);
+        assertSame(someTaskExecutorWithNameThatNeverWillBeQualifier, exportExecutor);
+    }
+}
