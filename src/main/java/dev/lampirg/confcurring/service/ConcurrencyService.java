81c47ad5297408c082cfe896fb594320015e77bb
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-31 12:51:17 +0300

    create ConcurrencyService

---
+++ src/main/java/dev/lampirg/confcurring/service/ConcurrencyService.java
@@ -0,0 +1,35 @@
+package dev.lampirg.confcurring.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConcurrencyService {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    private final TaskExecutor exportExecutor;
+    private final TaskExecutor importExecutor;
+
+
+    public ConcurrencyService(TaskExecutor exportExecutor,
+                              TaskExecutor importExecutor) {
+        this.exportExecutor = exportExecutor;
+        this.importExecutor = importExecutor;
+    }
+
+    public void doExport() {
+        exportExecutor.execute(() ->
+                logger.info("Doing export. Thread name: {}", Thread.currentThread().getName())
+        );
+    }
+
+    public void doImport() {
+        importExecutor.execute(() ->
+                logger.info("Doing import. Thread name: {}", Thread.currentThread().getName())
+        );
+    }
+}
