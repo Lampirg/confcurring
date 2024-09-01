e7e12f12f26629908fd053f787f955a30c869de6
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-09-01 09:26:47 +0300

    create test concurrent properties

--- src/test/java/dev/lampirg/confcurring/config/SharedConcurrentConfigurationTest.java
+++ src/test/java/dev/lampirg/confcurring/config/SharedConcurrentConfigurationTest.java
@@ -1,5 +1,6 @@
 package dev.lampirg.confcurring.config;
 
+import dev.lampirg.confcurring.factory.YamlPropertySourceFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(properties = "concurrent.single-thread=false")
-class ConcurrentConfigurationTest {
+@SpringBootTest
+@TestPropertySource(value = "/config/concurrent-shared.yaml", factory = YamlPropertySourceFactory.class)
+class SharedConcurrentConfigurationTest {
 
     @Autowired
     @Qualifier("exportExecutor")
